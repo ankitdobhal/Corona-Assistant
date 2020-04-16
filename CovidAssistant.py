6d5221db5c782b4aab7593a3799cf498f4460af4
ankitdobhal <dobhal.ankit@protonmail.com>
2020-04-16 21:04:59 +0530

    Added assistant file

---
+++ CovidAssistant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests as req
+import json
+from os import system
+from spider.Spiders import spiderIndia
+from spider.Spiders import spiderworld
+
+if __name__ == '__main__':
+	main()
